322f4f96ec54ed268a2b43b35762536ecee03125
Jeremy Biffis <jbiffis@adminisatorsmbp.lan>
2018-12-17 21:22:51 -0500

    Updated the fatasy team model

--- src/models/api-fantasy-teams.model.js
+++ src/models/api-fantasy-teams.model.js
@@ -6,10 +6,25 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const apiFantasyTeams = new Schema({
-    text: { type: String, required: true }
+    owner: { type: Object, required: true },
+    teamName: {type: String, required: true },
+    currentPlayers: {type: Object, required: false},  // when a player gets added, put them here.
+    pastPlayers: {type: Object, required: false},   // When a player gets dropped, move him from current to past.  Yes there can be dupes.
+    pointSummary: {type: Object, required: false},  // Every day add a new line with each current players points from today's games
+    teamPoints: {type: Object, required: false}   // Every day add a new item with current totals.
   }, {
     timestamps: true
   });
 
   return mongooseClient.model('apiFantasyTeams', apiFantasyTeams);
 };
+
+/**********************************************
+ * playerModes:
+ * {fullname, currentTeam, position, nhl_id, playerId, dateAcquired, dateRemoved}
+ * 
+ * pointSummary: 
+ * {fullname, nhl_id, player_id, position date, stats:{all of the stat categories they care about eg: G, P, SH, etc}}
+ * 
+ * 
+ */
