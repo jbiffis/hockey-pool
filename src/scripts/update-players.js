bddf305575ef7699b09fce46110a50a9890db1c8
Jeremy Biffis <jeremy@biffis.com>
2018-12-17 21:58:27 -0500

    Updated list of teams

--- src/scripts/update-players.js
+++ src/scripts/update-players.js
@@ -14,13 +14,38 @@ var baseOptions = {
   uri: 'https://statsapi.web.nhl.com/api/v1',
   json: true // Automatically parses the JSON string in the response
 };
-
-//TODO: Jordan please fill in the rest of this information using https://statsapi.web.nhl.com/api/v1/teams/4
 var teams = [
   {id: 1, teamName: 'New Jersey Devils', shortName: 'NJD'},
   {id: 2, teamName: 'New York Islanders', shortName: 'NYI'},
   {id: 3, teamName: 'New York Rangers', shortName: 'NYR'},
-  {id: 4, teamName: 'Philadelphia Flyers', shortName: 'PHI'}
+  {id: 4, teamName: 'Philadelphia Flyers', shortName: 'PHI'},
+  {id: 5, teamName: 'Pitsburgh Penguins', shortName: 'PIT'},
+  {id: 6, teamName: 'Boston Bruins', shortName: 'BOS'},
+  {id: 7, teamName: 'Buffalo Sabres', shortName: 'BUF'},
+  {id: 8, teamName: 'Montreal Canadiens', shortName: 'MTL'},
+  {id: 9, teamName: 'Ottawa Senators', shortName: 'OTT',},
+  {id:10, teamName: 'Toronto Maple Leafs', shortName: 'TOR'},
+  {id:12, teamName: 'Carolina Hurrcianes', shortName: 'CAR'},    
+  {id:13, teamName: 'Florida Panthers', shortName: 'FLA'},   
+  {id:14, teamName: 'Tampa Bay Lightning', shortName: 'TBL'},   
+  {id:15, teamName: 'Washington Capitals', shortName: 'WSH'},   
+  {id:16, teamName: 'Chicago Blackhawks', shortName: 'CHI'},   
+  {id:17, teamName: 'Detroit Red Wings', shortName: 'DET'},   
+  {id:18, teamName: 'Nashville Predators', shortName: 'NSH'},   
+  {id:19, teamName: 'St. Louis Blues', shortName: 'STL'},   
+  {id:20, teamName: 'Calgary Flames', shortName: 'CGY'},   
+  {id:21, teamName: 'Colorado Avalanche', shortName: 'COL'},   
+  {id:22, teamName: 'Edmonton Oilers', shortName: 'EDM'},   
+  {id:23, teamName: 'Vancouver Canucks', shortName: 'VAN'},   
+  {id:24, teamName: 'Anaheim Ducks', shortName: 'ANA'},   
+  {id:25, teamName: 'Dallas Stars', shortName: 'DAL'},   
+  {id:26, teamName: 'Los Angeles Kings', shortName: 'LAK'},   
+  {id:28, teamName: 'San Jose Sharks', shortName: 'SJS'},   
+  {id:29, teamName: 'Columbus Blue Jackets', shortName: 'CBJ'},   
+  {id:30, teamName: 'Minnesota Wild', shortName: 'MIN'},   
+  {id:52, teamName: 'Winnepeg Jets', shortName: 'WPG'},  
+  {id:53, teamName: 'Arizona Coyotes', shortName: 'ARI'},      
+  {id:54, teamName: 'Vegas Golden Knights', shortName: 'VGK'}, 
 ]
 
 //kick things off
@@ -32,7 +57,8 @@ function getPlayers () {
   return Promise.each(teams, team => {
     options = Object.assign({}, baseOptions);   //copy the base options object so we can modify
     options.uri = options.uri + '/teams/' + team.id + '/roster';  //set the team.
-
+    options.team = team.shortName;
+    
     // call the nhl api
     return makeCall(options)
       .then(players => {    // we get back a list of players on this team
@@ -42,7 +68,8 @@ function getPlayers () {
             fullName: player.person.fullName,
             nhl_id: player.person.id,
             number: player.jerseyNumber,
-            primaryPosition: player.position.abbreviation
+            primaryPosition: player.position.abbreviation,
+            currentTeam: options.team
           })
           .catch(err => {
             console.log(err);
