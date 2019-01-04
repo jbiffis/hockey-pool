e8a795aee058a8637f9984266e0270ad305ea58a
Jeremy Biffis <jeremy@biffis.com>
2019-01-04 15:43:35 -0500

    - Updated Users routes/service - Renamed things to match the forrect format (/api/users) - Added some user tests in postman

--- src/models/api-users.model.js
+++ src/models/api-users.model.js
@@ -1,22 +1,20 @@
-// users-model.js - A mongoose model
+// api-users-model.js - A mongoose model
 //
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
-  const users = new mongooseClient.Schema({
+  const apiUsers = new mongooseClient.Schema({
   
-    email: {type: String, unique: true},
+    email: {type: String, unique: true, lowercase: true},
     password: { type: String },
   
-  
     googleId: { type: String },
-  
     facebookId: { type: String },
   
   }, {
     timestamps: true
   });
 
-  return mongooseClient.model('users', users);
+  return mongooseClient.model('apiUsers', apiUsers);
 };
--- src/services/index.js
+++ src/services/index.js
@@ -3,8 +3,8 @@ const apiTeams = require('./api-teams/api-teams.service.js');
 const apiActions = require('./api-actions/api-actions.service.js');
 const apiLeagueTransactions = require('./api-league-transactions/api-league-transactions.service.js');
 const apiLeague = require('./api-league/api-league.service.js');
-const users = require('./users/users.service.js');
 const apiPlayerDailyStats = require('./api-player-daily-stats/api-player-daily-stats.service.js');
+const apiUsers = require('./api-users/api-users.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(apiPlayers);
@@ -12,6 +12,6 @@ module.exports = function (app) {
   app.configure(apiActions);
   app.configure(apiLeagueTransactions);
   app.configure(apiLeague);
-  app.configure(users);
   app.configure(apiPlayerDailyStats);
+  app.configure(apiUsers);
 };
